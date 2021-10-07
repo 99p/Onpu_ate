7b0a91797182866549cf8b33485eb17db6da304d
99p <poccori@gmail.com>
2021-10-07 22:52:43 +0900

    Initial Commit

---
+++ Onpu_ate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Onpu_ate
+//
+//  Created by macboy on 2021/10/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Onpu_ate/Onpu_ateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Onpu_ateApp.swift
+//  Onpu_ate
+//
+//  Created by macboy on 2021/10/07.
+//
+
+import SwiftUI
+
+@main
+struct Onpu_ateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

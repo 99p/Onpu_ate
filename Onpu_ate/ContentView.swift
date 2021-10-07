e4920ab91241b755ffaec34599f4c84ae1083a57
99p <poccori@gmail.com>
2021-10-07 23:28:21 +0900

    add ogpu, gosenfu

--- Onpu_ate/ContentView.swift
+++ Onpu_ate/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+        Image("gosenfu")
+            .resizable()
+            .scaledToFill()
+            .frame(width: 300, height: 150)
+        Image("tama")
+        }
     }
 }
 
